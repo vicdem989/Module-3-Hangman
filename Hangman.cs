4b8e7128b6a03cabd3427d49ba76c59d322a97a6
vicdem989 <vicdemhoy@hotmail.com>
2023-10-03 21:09:09 +0200

    Working on hint
    Check if the hint is already guessed before printing

--- Hangman.cs
+++ Hangman.cs
@@ -1,4 +1,6 @@
+using System.Data;
 using System.Net;
+using System.Reflection.Metadata.Ecma335;
 using LANGUAGE;
 using Microsoft.VisualBasic;
 
@@ -7,7 +9,7 @@ namespace GAME
     public class Game
     {
         private const bool DEBUG = true;
-        private static string[] WORD_LIST = new[] { "car", "cat", "dog", "elephant", "fish", "giraffe", "horse", "monkey", "panda", "sheep", "tiger", "zebra", "ant", "bee", "marmelade", "computer", "dax", "sjøbanan" };
+        private static string[] WORD_LIST = new[] { "car", "cat", "dog", "elephant", "fish", "giraffe", "horse", "monkey", "panda", "sheep", "tiger", "zebra", "ant", "bee", "marmelade", "computer", "dax", "sjobanan" };
         private static int MAX_ATTEMPTS = Graphics.HANGMAN_STATE_DRAWINGS.Length - 1;
         private static int currentAttemptsAtGuessingWord = 0;
         private static string word = PickRandomItemFromList(WORD_LIST);
@@ -15,19 +17,22 @@ namespace GAME
         private static string currentDrawing = Graphics.HANGMAN_STATE_DRAWINGS[currentAttemptsAtGuessingWord];
         private static bool isPlaying = true;
         private static List<char> charGuessed = new List<char>();
+        private static char guess = '\0';
+        private static Random randomNumber = new Random();
+        private static char letterHint = '\0';
 
 
         static void Main(string[] args)
         {
             DifferentLanguages.appText = DifferentLanguages.chooseLanguage();
-            StartGame();
+            GameLogic();
         }
 
-        private static void StartGame()
+        private static void GameLogic()
         {
+
             while (isPlaying)
             {
-                //Clear();
                 if (DEBUG)
                 {
                     Print(DifferentLanguages.appText.Word + $"{word}");
@@ -36,9 +41,10 @@ namespace GAME
                 Print(currentDrawing);
                 Print(DifferentLanguages.appText.GuessLetter, newLine: false);
 
-                char guess = ReadChar();
-                if (word.Contains(guess))
+                guess = ReadChar();
+                if (word.Contains(guess) && !charGuessed.Contains(guess))
                 {
+                    Clear();
                     for (int i = 0; i < word.Length; i++)
                     {
                         if (word[i] == guess)
@@ -52,28 +58,57 @@ namespace GAME
                     Clear();
                     currentAttemptsAtGuessingWord++;
                     currentDrawing = Graphics.HANGMAN_STATE_DRAWINGS[currentAttemptsAtGuessingWord];
-                    charGuessed.Add(Convert.ToChar(guess));
-                    for (int i = 0; i < charGuessed.Count; i++)
-                    {
-                        Console.Write(charGuessed[i] + " ");
-                    }
-                    Console.WriteLine();
-
                 }
-
-                if (wordDisplay == word)
-                {
-                    //Clear();
-                    Print(DifferentLanguages.appText.YouWon + $"{word}");
-                    isPlaying = false;
-                }
-                else if (currentAttemptsAtGuessingWord == MAX_ATTEMPTS)
-                {
-                    //clear();
-                    Print(DifferentLanguages.appText.YouLost + $"{word}");
-                    isPlaying = false;
-                }
+                
+                //Print("There may be a " + LetterHint().ToString() + " in the word.");
+                GuessedChars();
+                Print(letterHint + LetterHint().ToString());
+                checkPlayStatus();
             }
+        }
+
+        private static bool LetterHint()
+        { //Give random hint and check if the letter is already guesses
+            int minNumber = 1;
+            int maxNumber = wordDisplay.Length - 1;
+            
+            letterHint = word[randomNumber.Next(minNumber, maxNumber)];
+            return charGuessed.Any(x => x == letterHint);
+            /*while(checkIfHintAlreadyGuessed) {
+                letterHint = word[randomNumber.Next(minNumber, maxNumber)];
+            }
+            return letterHint;   */
+        }
+        
+
+        private static void checkPlayStatus()
+        {
+            if (wordDisplay == word)
+            {
+                Print(currentDrawing);
+                Print(DifferentLanguages.appText.YouWon + $"{word}");
+                isPlaying = false;
+            }
+            else if (currentAttemptsAtGuessingWord == MAX_ATTEMPTS)
+            {
+
+                Print(currentDrawing);
+                Print(DifferentLanguages.appText.YouLost + $"{word}");
+                isPlaying = false;
+            }
+        }
+
+        private static void GuessedChars()
+        {
+            Print("", true);
+            Print("Already guessed chars: ", false);
+            if (!charGuessed.Contains(guess))
+                charGuessed.Add(Convert.ToChar(guess));
+            for (int i = 0; i < charGuessed.Count; i++)
+            {
+                Print(charGuessed[i] + " ", false);
+            }
+            Console.WriteLine();
         }
 
 
@@ -118,3 +153,5 @@ namespace GAME
         }
     }
 }
+
+
